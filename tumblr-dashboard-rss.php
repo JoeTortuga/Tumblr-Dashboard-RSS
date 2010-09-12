39fc2b57b7f70bb4e092efaca6d7aad3ac0dc510
Josh Mock <josh@joshmock.com>
2010-09-12 14:45:26 -0500

    Added license info to files.

--- tumblr-dashboard-rss.php
+++ tumblr-dashboard-rss.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+LICENSE INFO
+This program is free software. It comes without any warranty, to the extent permitted by applicable law. You can redistribute it and/or modify it under the terms of the Do What The Fuck You Want To Public License, Version 2, as published by Sam Hocevar. See http://sam.zoy.org/wtfpl/COPYING for more details.
+
+CONTACT INFO
+Created by Josh Mock
+Website: http://joshmock.com
+Email: josh@joshmock.com
+*/
+
 // Authorization info
 $tumblr_email    = 'test@example.com';
 $tumblr_password = 'password';
